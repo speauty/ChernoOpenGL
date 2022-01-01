45f2755585221592878d99f5d8ff192c4af034de
speauty <speauty@163.com>
2022-01-01 15:33:54 +0800

    P7-写了两个简单的着色器

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -2,6 +2,49 @@
 #include "GL/glew.h"
 #include "GLFW/glfw3.h"
 
+static unsigned int CompileShader(unsigned int type, const std::string& source)
+{
+    unsigned int id = glCreateShader(type); /* 创建对应类型的着色器 */
+    const char* src = source.c_str();
+    glShaderSource(id, 1, &src, nullptr); /* 设置着色器源码 */
+    glCompileShader(id); /* 编译着色器 */
+
+    /* 编译错误处理 */
+    int result;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &result); // 获取当前着色器编译状态
+    if (result == GL_FALSE) {
+        int length;
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length); // 获取日志长度
+        char* msg = (char*)_malloca(length * sizeof(char)); /* Cherno这里采用的alloca, 根据IDE提示, 我这里改成了_malloca函数 */
+        glGetShaderInfoLog(id, length, &length, msg); // 获取日志信息
+        std::cout << "Failed to compile " << (type == GL_VERTEX_SHADER ? "vertex":"fragment") << " shader!" << std::endl;
+        std::cout << msg << std::endl;
+        glDeleteShader(id); // 删除着色器
+        return 0;
+    }
+
+    return id;
+}
+
+static int CreateShader(const std::string& vertexShader, const std::string& fragmentShader)
+{
+    unsigned int program = glCreateProgram(); /* 创建程序 */
+    unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+    unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+    
+    /* 将着色器附加到程序上 */
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program); /* 链接程序 */
+    glValidateProgram(program); /* 验证 */
+
+    /* 删除着色器 */
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    return program;
+}
+
 int main(void)
 {
     GLFWwindow* window;
@@ -41,6 +84,28 @@ int main(void)
     glEnableVertexAttribArray(0); /* 激活顶点属性-索引0-位置 */
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0); /* 设置顶点属性-索引0 */
 
+    std::string vertexShader = 
+        "#version 330 core\n"
+        "\n"
+        "layout(location = 0)in vec4 position;"
+        "\n"
+        "void main()\n"
+        "{\n"
+        " gl_Position = position;\n"
+        "}\n";
+
+    std::string fragmentShader =
+        "#version 330 core\n"
+        "\n"
+        "layout(location = 0)out vec4 color;"
+        "\n"
+        "void main()\n"
+        "{\n"
+        " color = vec4(1.0, 0.0, 0.0, 1.0);\n"
+        "}\n";
+    unsigned int shader = CreateShader(vertexShader, fragmentShader);
+    glUseProgram(shader); /* 使用着色器程序 */
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window)) {
         /* Render here */
@@ -54,6 +119,7 @@ int main(void)
         /* Poll for and process events */
         glfwPollEvents();
     }
+    glDeleteProgram(shader); // 删除着色器程序
 
     glfwTerminate();
     return 0;

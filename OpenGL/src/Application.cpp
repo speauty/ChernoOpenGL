8d10baeb9b1270073c8643188e1125e97daccb0c
speauty <speauty@163.com>
2022-01-01 11:45:40 +0800

    P5-设置顶点属性布局, 并额外补充注释

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -26,6 +26,7 @@ int main(void)
     std::cout << "Status: Using GLEW " << glewGetString(GLEW_VERSION) << std::endl;
     std::cout << "Status: Using GL " << glGetString(GL_VERSION) << std::endl;
 
+    /* 顶点位置浮点型数组 */
     float positions[6] = {
         -0.5f, -0.5f,
         0.0f, 0.5f,
@@ -33,16 +34,19 @@ int main(void)
     };
 
     unsigned int buffer;
-    glGenBuffers(1, &buffer);
-    glBindBuffer(GL_ARRAY_BUFFER, buffer);
-    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+    glGenBuffers(1, &buffer); /* 生成缓冲区 */
+    glBindBuffer(GL_ARRAY_BUFFER, buffer); /* 绑定缓冲区 */
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW); /* 设置缓冲区数据 */
+    
+    glEnableVertexAttribArray(0); /* 激活顶点属性-索引0-位置 */
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0); /* 设置顶点属性-索引0 */
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window)) {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glDrawArrays(GL_TRIANGLES, 0, 3 );
+        glDrawArrays(GL_TRIANGLES, 0, 3); // 绘制
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
